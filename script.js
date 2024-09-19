aa31a6d5768eb0d893f76319735f4c912b369b49
Samy <hernandezbarriossamuel@gmail.com>
2024-09-18 20:52:35 -0600

    Primer comit de la clase del 18/09/2024

---
+++ script.js
@@ -0,0 +1,31 @@
+alert("ola");
+/* 
+18/09/2024 /Hernandez Barrios Samuel Rodrigo
+Ola
+*/
+
+// Declaracion de variables
+let nombre = "Samuel";
+const PI = 3.1416;
+var edad = 25;
+
+/*Tipos de datos
+De texto (String): "Hola" , 'mundo'
+Numero (number): 5,3,14
+Booleano (boolean): true. false
+*/
+
+//Ejemplo
+let mensaje = "Hola mundo";
+let numero = 42;
+let estudiante = true;
+
+
+//Operaciones matematicas
+let a = 5;
+let b = 2;
+
+let suma = a+b;
+let resta = a-b;
+let multi = a*b;
+let division = a/b;
