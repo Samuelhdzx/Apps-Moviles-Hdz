d9e69301a373a4404f5dba7740fbe752f2525e14
Samy <hernandezbarriossamuel@gmai.com>
2024-09-23 20:36:24 -0600

    Subida de archivos

---
+++ Clase_230924/Eventos/EventosyEscuchadeEventos.js
@@ -0,0 +1,11 @@
+// Obtener el botón del DOM
+const botonColor = document.getElementById('cambiarColor');
+
+botonColor.addEventListener('click', () =>{
+    const colorAleatorio = 
+})
+
+// Anadir un listener al botón para manejar el evento click botonColor.addEventListener('click',	(
+// Generar un color aleatorio
+
+// Cambiar el color de fondo del body document.body.style.backgroundColor	colorAleatorio;
