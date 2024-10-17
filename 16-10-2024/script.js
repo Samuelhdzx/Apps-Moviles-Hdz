b05470180306c6b1f293dc37eb385fa5037ee5c4
Samy <hernandezbarriossamuel@gmail.com>
2024-10-16 20:42:18 -0600

    Clase 16-10-2024

--- 16-10-2024/script.js
+++ 16-10-2024/script.js
@@ -2,6 +2,6 @@ console.log("ola");
 
 const h1 = document.createElement("h1");
 
-h1.textContent = "Hola"
+h1.textContent = "Hodfdfla"
 
-document.body.appendChild(h1); 
+document.body.appendChild(h1);
